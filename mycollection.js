8e51eefc3965b32ed5435f19d605892270dd97be
Anthony Onyejesi <anthonyonyejesi@Anthonys-MacBook-Pro.local>
2020-04-01 21:18:48 +0100

    finished db

--- mycollection.js
+++ mycollection.js
@@ -5,4 +5,14 @@ var url = "mongodb://localhost:27017/emeka_black_beard_";
 
 // var Admin = "emeka[black beard]";
 
-MongoClient.connect(url, )
+MongoClient.connect(url, {useNewUrlParser: true, useUnifiedTopology: true }, function(err ,db){
+    var dbmi = db.db("emeka_black_beard_");
+
+    if (err) throw err;
+    console.log("Database created by")
+    dbmi.createCollection("interns", function(err ,result){
+        if (err) throw err;
+        console.log("Intern Coollection has been created!");
+        db.close
+    });
+});
