d6353bb76eb9adecd981d0d636126f742b2d4898
Anthony Onyejesi <anthonyonyejesi@Anthonys-MacBook-Pro.local>
2020-04-01 21:22:36 +0100

    ok na

--- mongo_db.js
+++ mongo_db.js
@@ -4,5 +4,5 @@ var url = "mongodb://localhost:27017/emeka_black_beard_";
 MongoClient.connect(url, function(err, db) {
     if (err) throw err;
     console.log("Database created by emeka[black beard]");
-    db.close(); 
+    db.close();
     });
